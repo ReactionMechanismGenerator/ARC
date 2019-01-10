10fc86a9cf989bef07f00e00999579968257ac41
alongd <alongd@mit.edu>
2019-01-10 09:40:15 -0500

    Testing: Added gaussian compisite freq parsing test
    Added to test_parse_frequencies

--- arc/parserTest.py
+++ arc/parserTest.py
@@ -22,17 +22,22 @@ class TestParser(unittest.TestCase):
     """
 
     def test_parse_frequencies(self):
-        """Test parsing of QChem frequencies"""
+        """Test frequency parsing"""
         no3_path = os.path.join(arc_path, 'arc', 'testing', 'NO3_freq_QChem_fails_on_cclib.out')
         c2h6_path = os.path.join(arc_path, 'arc', 'testing', 'C2H6_freq_Qchem.out')
+        so2oo_path = os.path.join(arc_path, 'arc', 'testing', 'SO2OO_CBS-QB3.log')
         no3_freqs = parser.parse_frequencies(path=no3_path, software='QChem')
         c2h6_freqs = parser.parse_frequencies(path=c2h6_path, software='QChem')
+        so2oo_freqs = parser.parse_frequencies(path=so2oo_path, software='Gaussian')
         self.assertTrue(np.array_equal(no3_freqs,
                                        np.array([-390.08, -389.96, 822.75, 1113.23, 1115.24, 1195.35], np.float64)))
         self.assertTrue(np.array_equal(c2h6_freqs,
                                        np.array([352.37, 847.01, 861.68, 1023.23, 1232.66, 1235.04, 1425.48, 1455.31,
                                                  1513.67, 1518.02, 1526.18, 1526.56, 3049.78, 3053.32, 3111.61, 3114.2,
                                                  3134.14, 3136.8], np.float64)))
+        self.assertTrue(np.array_equal(so2oo_freqs,
+                                       np.array([302.51, 468.1488, 469.024, 484.198, 641.0067, 658.6316,
+                                                 902.2888, 1236.9268, 1419.0826], np.float64)))
 
 ################################################################################
 
