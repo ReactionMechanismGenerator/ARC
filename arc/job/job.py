73d5ecce43ffc5f5e6fdf145ed10640426f6250c
alongd <alongd@mit.edu>
2019-03-15 08:59:41 -0400

    Correctly assign mem-per-cpu for molpro submit scripts
    Cannot be larger than 16000 MB per cpu

--- arc/job/job.py
+++ arc/job/job.py
@@ -334,7 +334,7 @@ class Job(object):
         else:
             raise JobError('Could not determine format for maximal job time')
         self.submit = submit_scripts[servers[self.server]['cluster_soft']][self.software.lower()].format(
-            name=self.job_server_name, un=un, t_max=t_max, mem_cpu=self.memory * 150)
+            name=self.job_server_name, un=un, t_max=t_max, mem_cpu=min(int(self.memory * 150), 16000))
         # Memory convertion: multiply MW value by 1200 to conservatively get it in MB, then divide by 8 to get per cup
         if not os.path.exists(self.local_path):
             os.makedirs(self.local_path)
