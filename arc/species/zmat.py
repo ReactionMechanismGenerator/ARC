6d34444a9999240c168c549bdc6a35372e0f64a6
oscarwumit <oscarwu@mit.edu>
2019-03-15 16:37:32 -0400

    self.zmat from/to xyz or Z matrix

--- arc/species/zmat.py
+++ arc/species/zmat.py
@@ -30,12 +30,13 @@ class ZMatrix(object):
     ====================== ============= ===============================================================================
     `xyz`                   ``str``      Species xyz representation
     `zmat_str`              ``str``      Species zmat representation
-    `zmat`                  ``list``      List of zmat atom items
+    `zmat`                  ``list``     List of zmat atom items
     ====================== ============= ===============================================================================
 
     """
+
     def __init__(self, xyz=None, zmat_str=None):
-        self.xyz = standardize_xyz_string(xyz)
+        self.xyz = standardize_xyz_string(xyz) if xyz is not None else None
         self.zmat_str = zmat_str
         self.zmat = []
         if self.xyz is None and self.zmat_str is None:
@@ -49,18 +50,50 @@ class ZMatrix(object):
         """
         Construct self.zmat from input self.xyz
         """
+
         xyz_matrix, symbols, x, y, z = get_xyz_matrix(self.xyz)
 
     def from_zmat_str(self):
         """
         Construct self.zmat from input self.zmat_str
         """
-        for line in self.zmat_str.splitlines():
-            distance, angle, dihedral = None, None, None
 
+        for index, line in enumerate(self.zmat_str.splitlines()):
+            symbol = line.split()[0]
+            if index == 0:
+                indices_list = [index]
+                self.zmat.append(ZMatrixAtom(symbol, indices_list))
+            elif index == 1:
+                distance_reference_atom_index = line.split()[1]
+                indices_list = [index, distance_reference_atom_index]
+                distance = line.split()[2]
+                self.zmat.append(ZMatrixAtom(symbol, indices_list, distance))
+            elif index == 2:
+                distance_reference_atom_index = line.split()[1]
+                angle_reference_atom_index = line.split()[3]
+                indices_list = [index, distance_reference_atom_index, angle_reference_atom_index]
+                distance = line.split()[2]
+                angle = line.split()[4]
+                self.zmat.append(ZMatrixAtom(symbol, indices_list, distance, angle))
+            elif index >= 3:
+                distance_reference_atom_index = line.split()[1]
+                angle_reference_atom_index = line.split()[3]
+                dihedral_reference_atom_index = line.split()[5]
+                indices_list = [index, distance_reference_atom_index, angle_reference_atom_index,
+                                dihedral_reference_atom_index]
+                distance = line.split()[2]
+                angle = line.split()[4]
+                dihedral = line.split()[6]
+                self.zmat.append(ZMatrixAtom(symbol, indices_list, distance, angle, dihedral))
 
-
-
+    def to_zmat_str(self):
+        """
+        Convert self.zmat to a Z-matrix list
+        :return: zmat_list
+        """
+        zmat_list_tmp = [obj.to_str() for obj in self.zmat]
+        zmat_list = "\n".join(zmat_list_tmp)
+        return zmat_list
 
 class ZMatrixAtom(object):
     """
@@ -95,8 +128,18 @@ class ZMatrixAtom(object):
             raise ZMatrixError('Length of indices_list has to be at most 4')
         self.index = indices_list[0]
 
+    def to_str(self):
+        """
+        Convert a ZMatrixAtom to a string
+        :return: zmat_atom_str
+        """
 
-
-
-
-
+        indices_list_full = [None]*4
+        indices_list_full[:len(self.indices_list)] = self.indices_list
+        zmat_atom_list = [self.symbol,
+                          indices_list_full[1], self.distance,
+                          indices_list_full[2], self.angle,
+                          indices_list_full[3], self.dihedral]
+        zmat_atom_list_tmp = [entry for entry in zmat_atom_list if entry is not None]
+        zmat_atom_str = " ".join(str(entry) for entry in zmat_atom_list_tmp)
+        return zmat_atom_str
