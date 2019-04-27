3ab10c50a0148316cb630904bd5772f7a94c7ffe
alongd <alongd@mit.edu>
2019-04-26 21:59:22 -0400

    Tests: Submit tests

---
+++ arc/job/submitTest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains unit tests of the arc.job.submit module
+"""
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+import unittest
+
+from arc.job.submit import submit_scripts
+
+################################################################################
+
+
+class TestSubmit(unittest.TestCase):
+    """
+    Contains unit tests for the submit module
+    """
+
+    def test_servers(self):
+        """Test server keys in submit_scripts"""
+        for server in submit_scripts.keys():
+            self.assertTrue(server in ['pharos', 'c3ddb', 'rmg'])
+
+
+################################################################################
+
+if __name__ == '__main__':
+    unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
