b4b9e83afc48f90ebed72162b4f4da22051f2505
alongd <alongd@mit.edu>
2019-05-06 16:39:05 -0400

    Tests: Job Local Test

---
+++ arc/job/localTest.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains unit tests of the arc.job.local module
+"""
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+import unittest
+import os
+import shutil
+import datetime
+
+import arc.job.local as local
+from arc.settings import arc_path
+
+################################################################################
+
+
+class TestLocal(unittest.TestCase):
+    """
+    Contains unit tests for the local module
+    """
+
+    def test_execute_command(self):
+        """Test executing a local command"""
+        command1 = 'ls'
+        out1 = local.execute_command(command1)
+        self.assertIsInstance(out1, tuple)
+        self.assertIsInstance(out1[0], list)
+        self.assertEqual(out1[1], '')
+        self.assertIn('arc', out1[0])
+        self.assertIn('ARC.py', out1[0])
+        self.assertIn('environment.yml', out1[0])
+
+    def test_get_last_modified_time(self):
+        """Test the get_last_modified_time() function"""
+        path = os.path.join(arc_path, 'ARC.py')
+        t = local.get_last_modified_time(path)
+        self.assertIsInstance(t, datetime.datetime)
+
+    def test_rename_output(self):
+        """Test the rename_output() function"""
+        path1 = os.path.join(arc_path, 'scratch', 'input.log')
+        path2 = os.path.join(arc_path, 'scratch', 'output.out')
+        if not os.path.exists(os.path.join(arc_path, 'scratch')):
+            os.makedirs(os.path.join(arc_path, 'scratch'))
+        with open(path1, 'w'):
+            pass
+        local.rename_output(local_file_path=path2, software='gaussian')
+        self.assertFalse(os.path.isfile(path1))
+        self.assertTrue(os.path.isfile(path2))
+        shutil.rmtree(os.path.join(arc_path, 'scratch'))
+
+################################################################################
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
