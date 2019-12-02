346d8e42cc13a42fbe1d95f7a1a84c19223ce8f5
Alon Grinberg Dana <alongd@mit.edu>
2019-12-02 10:43:24 -0500

    BugFix: Conformer wells run from 0 to 360, not -180 to 180
    This bug affected correct symmetry assingment

--- arc/species/conformers.py
+++ arc/species/conformers.py
@@ -1344,8 +1344,8 @@ def get_wells(label, angles, blank=20):
     if not angles:
         raise ConformerError('Cannot determine wells without angles for {0}'.format(label))
     new_angles = angles
-    if angles[0] < -180 + blank and angles[-1] > 180 - blank:
-        # relocate the first chunk of data to the end, the well seems to include the  +180/-180 degrees point
+    if angles[0] < 0 + blank and angles[-1] > 360 - blank:
+        # relocate the first chunk of data at the end, the well seems to include the  +180/-180 degrees point
         for i, angle in enumerate(angles):
             if i > 0 and abs(angle - angles[i - 1]) > blank:
                 part2 = angles[:i]
