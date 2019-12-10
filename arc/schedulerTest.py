d11e7a32c5c5ff6d285789b95c1779e663de15d9
Oscar Wu <oscarwu@mit.edu>
2019-12-09 20:28:22 -0500

    Tests: change job_level_of_theory to dictionary

--- arc/schedulerTest.py
+++ arc/schedulerTest.py
@@ -43,14 +43,16 @@ H      -1.82570782    0.42754384   -0.56130718"""
         cls.spc3 = ARCSpecies(label='CtripCO', smiles='C#CO', xyz=xyz1)
         xyz2 = {'symbols': ('C',), 'isotopes': (12,), 'coords': ((0.0, 0.0, 0.0),)}
         cls.job1 = Job(project='project_test', ess_settings=cls.ess_settings, species_name='methylamine',
-                       xyz=xyz2, job_type='conformer', conformer=0, level_of_theory='b97-d3/6-311+g(d,p)',
+                       xyz=xyz2, job_type='conformer', conformer=0,
+                       job_level_of_theory_dict={'method': 'b97-d3', 'basis': '6-311+g(d,p)'},
                        multiplicity=1, project_directory=cls.project_directory, job_num=101)
         cls.job2 = Job(project='project_test', ess_settings=cls.ess_settings, species_name='methylamine',
-                       xyz=xyz2, job_type='conformer', conformer=1, level_of_theory='b97-d3/6-311+g(d,p)',
+                       xyz=xyz2, job_type='conformer', conformer=1,
+                       job_level_of_theory_dict={'method': 'b97-d3', 'basis': '6-311+g(d,p)'},
                        multiplicity=1, project_directory=cls.project_directory, job_num=102)
         cls.job3 = Job(project='project_test', ess_settings=cls.ess_settings, species_name='C2H6', xyz=xyz2,
-                       job_type='freq', level_of_theory='wb97x-d3/6-311+g(d,p)', multiplicity=1,
-                       project_directory=cls.project_directory, software='qchem', job_num=103)
+                       job_type='freq', job_level_of_theory_dict={'method': 'wb97x-d3', 'basis': '6-311+g(d,p)'},
+                       multiplicity=1, project_directory=cls.project_directory, software='qchem', job_num=103)
         cls.rmgdb = rmgdb.make_rmg_database_object()
         cls.job_types1 = {'conformers': True,
                           'opt': True,
@@ -103,7 +105,8 @@ H      -1.82570782    0.42754384   -0.56130718"""
         self.assertTrue(os.path.isfile(methylamine_conf_path))
         with open(methylamine_conf_path, 'r') as f:
             lines = f.readlines()
-        self.assertTrue('Conformers for methylamine, optimized at the b3lyp/6-31g(d,p) EmpiricalDispersion=GD3BJ level'
+        self.assertTrue("Conformers for methylamine, optimized at the {'auxiliary_basis': '', 'basis': '6-31g(d,p)', "
+                        "'method': 'b3lyp', 'dispersion': 'empiricaldispersion=gd3bj'} level"
                         in lines[0])
         self.assertEqual(lines[11], 'SMILES: CN\n')
         self.assertTrue('Relative Energy:' in lines[12])
