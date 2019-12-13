9f26e92e23b9036970e4701e89888a5491f3692a
Oscar Wu <oscarwu@mit.edu>
2019-12-13 13:30:33 -0500

    Tests: Orca troubleshoot tests

--- arc/job/trshTest.py
+++ arc/job/trshTest.py
@@ -96,6 +96,117 @@ class TestTrsh(unittest.TestCase):
         self.assertEqual(error, 'Unrecognized basis set 6-311G**')
         self.assertIn(' ? Basis library exhausted', line)  # line includes '\n'
 
+        # Orca
+
+        # test detection of a successful job
+        path = os.path.join(self.base_path['orca'], 'orca_successful_sp.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'done')
+        self.assertEqual(keywords, list())
+        self.assertEqual(error, '')
+        self.assertEqual(line, '')
+
+        # test detection of SCF energy diverge issue
+        path = os.path.join(self.base_path['orca'], 'orca_scf_blow_up_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['SCF'])
+        expected_error_msg = 'The SCF energy seems diverged during iterations. ' \
+                             'SCF energy after initial iteration is -1076.6615662471. ' \
+                             'SCF energy after final iteration is -20006124.68383977. ' \
+                             'The ratio between final and initial SCF energy is 18581.627979509627. ' \
+                             'This ratio is greater than the default threshold of 2. ' \
+                             'Please consider using alternative methods or larger basis sets.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('', line)
+
+        # test detection of insufficient memory causes SCF failure
+        path = os.path.join(self.base_path['orca'], 'orca_scf_memory_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['SCF', 'Memory'])
+        expected_error_msg = 'Orca suggests to increase per cpu core memory to 289 MB.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertEqual(' Error  (ORCA_SCF): Not enough memory available!', line)
+
+        # test detection of insufficient memory causes MDCI failure
+        path = os.path.join(self.base_path['orca'], 'orca_mdci_memory_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['MDCI', 'Memory'])
+        expected_error_msg = 'Orca suggests to increase per cpu core memory to 9718 MB.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('Please increase MaxCore', line)
+
+        # test detection of too many cpu cores causes MDCI failure
+        path = os.path.join(self.base_path['orca'], 'orca_too_many_cores.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['MDCI', 'cpu'])
+        expected_error_msg = 'Orca cannot utilize cpu cores more than electron pairs in a molecule. ' \
+                             'The maximum number of cpu cores can be used for this job is 10.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('parallel calculation exceeds', line)
+
+        # test detection of generic MDCI failure
+        path = os.path.join(self.base_path['orca'], 'orca_mdci_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['MDCI'])
+        expected_error_msg = 'MDCI error in Orca.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('ORCA finished by error termination in MDCI', line)
+
+        # test detection of multiplicty and charge combination error
+        path = os.path.join(self.base_path['orca'], 'orca_multiplicity_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['Input'])
+        expected_error_msg = 'The multiplicity and charge combination for species test are wrong.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('Error : multiplicity', line)
+
+        # test detection of input keyword error
+        path = os.path.join(self.base_path['orca'], 'orca_input_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['Syntax'])
+        expected_error_msg = 'There was keyword syntax error in the Orca input file. In particular, keywords ' \
+                             'XTB1 can either be duplicated or illegal. Please check your Orca ' \
+                             'input file template under arc/job/inputs.py. Alternatively, perhaps the level of ' \
+                             'theory or the job option is not supported by Orca in the format it was given.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('XTB1', line)
+
+        # test detection of basis set error (e.g., input contains elements not supported by specified basis)
+        path = os.path.join(self.base_path['orca'], 'orca_basis_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['Basis'])
+        expected_error_msg = 'There was a basis set error in the Orca input file. In particular, basis for atom type ' \
+                             'Br is missing. Please check if specified basis set supports this atom.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('There are no CABS', line)
+
+        # test detection of wavefunction convergence failure
+        path = os.path.join(self.base_path['orca'], 'orca_wavefunction_not_converge_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(
+            output_path=path, species_label='test', job_type='sp', software='orca')
+        self.assertEqual(status, 'errored')
+        self.assertEqual(keywords, ['Convergence'])
+        expected_error_msg = 'Specified wavefunction method is not converged. Please restart calculation with larger ' \
+                             'max iterations or with different convergence flags.'
+        self.assertEqual(error, expected_error_msg)
+        self.assertIn('This wavefunction IS NOT FULLY CONVERGED!', line)
 
     def test_trsh_ess_job(self):
         """Test the trsh_ess_job() function"""
@@ -175,9 +286,110 @@ class TestTrsh(unittest.TestCase):
         output_errors, ess_trsh_methods, remove_checkfile, level_of_theory, software, job_type, fine, trsh_keyword, \
         memory, shift, couldnt_trsh = trsh.trsh_ess_job(label, level_of_theory, server, job_status, job_type, software,
                                                         fine, memory_gb, num_heavy_atoms, ess_trsh_methods)
-
         self.assertIn('memory', ess_trsh_methods)
         self.assertAlmostEqual(memory, 222.15625)
+
+        #### test orca ####
+        ## orca: test 1 ##
+        # Test troubleshooting insufficient memory issue
+        # Automatically increase memory provided not exceeding maximum available memory
+        label = 'test'
+        level_of_theory = 'DLPNO ccsd(T)'
+        server = 'server1'
+        job_type = 'sp'
+        software = 'orca'
+        fine = True
+        memory_gb = 250
+        cpu_cores = 32
+        num_heavy_atoms = 20
+        ess_trsh_methods = ['memory']
+        path = os.path.join(self.base_path['orca'], 'orca_mdci_memory_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(output_path=path, species_label='TS', job_type='sp',
+                                                                  software=software)
+        job_status = {'keywords': keywords, 'error': error}
+        output_errors, ess_trsh_methods, remove_checkfile, level_of_theory, software, job_type, fine, trsh_keyword, \
+        memory, shift, cpu_cores, couldnt_trsh = trsh.trsh_ess_job(label, level_of_theory, server, job_status, job_type,
+                                                                   software, fine, memory_gb, num_heavy_atoms,
+                                                                   cpu_cores, ess_trsh_methods)
+        self.assertIn('memory', ess_trsh_methods)
+        self.assertEqual(cpu_cores, 32)
+        self.assertAlmostEqual(memory, 312)
+
+        ## orca: test 2 ##
+        # Test troubleshooting insufficient memory issue
+        # Automatically reduce cpu cores to ensure enough memory per core when maximum available memory is limited
+        label = 'test'
+        level_of_theory = 'DLPNO ccsd(T)'
+        server = 'server1'
+        job_type = 'sp'
+        software = 'orca'
+        fine = True
+        memory_gb = 250
+        cpu_cores = 32
+        num_heavy_atoms = 20
+        ess_trsh_methods = ['memory']
+        path = os.path.join(self.base_path['orca'], 'orca_mdci_memory_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(output_path=path, species_label='TS', job_type='sp',
+                                                                  software=software)
+        keywords.append('max_total_job_memory')
+        job_status = {'keywords': keywords, 'error': error}
+        output_errors, ess_trsh_methods, remove_checkfile, level_of_theory, software, job_type, fine, trsh_keyword, \
+        memory, shift, cpu_cores, couldnt_trsh = trsh.trsh_ess_job(label, level_of_theory, server, job_status, job_type,
+                                                                   software, fine, memory_gb, num_heavy_atoms,
+                                                                   cpu_cores, ess_trsh_methods)
+        self.assertIn('memory', ess_trsh_methods)
+        self.assertEqual(cpu_cores, 25)
+        self.assertAlmostEqual(memory, 245)
+
+        ## orca: test 3 ##
+        # Test troubleshooting insufficient memory issue
+        # Stop troubleshooting when ARC determined there is not enough computational resource to accomplish the job
+        label = 'test'
+        level_of_theory = 'DLPNO ccsd(T)'
+        server = 'server1'
+        job_type = 'sp'
+        software = 'orca'
+        fine = True
+        memory_gb = 1
+        cpu_cores = 32
+        num_heavy_atoms = 20
+        ess_trsh_methods = ['memory']
+        path = os.path.join(self.base_path['orca'], 'orca_mdci_memory_error.log')
+        status, keywords, error, line = trsh.determine_ess_status(output_path=path, species_label='TS', job_type='sp',
+                                                                  software=software)
+        keywords.append('max_total_job_memory')
+        job_status = {'keywords': keywords, 'error': error}
+        output_errors, ess_trsh_methods, remove_checkfile, level_of_theory, software, job_type, fine, trsh_keyword, \
+        memory, shift, cpu_cores, couldnt_trsh = trsh.trsh_ess_job(label, level_of_theory, server, job_status, job_type,
+                                                                   software, fine, memory_gb, num_heavy_atoms,
+                                                                   cpu_cores, ess_trsh_methods)
+        self.assertIn('memory', ess_trsh_methods)
+        self.assertEqual(couldnt_trsh, True)
+        self.assertLess(cpu_cores, 1)    # can't really run job with less than 1 cpu ^o^
+
+        ## orca: test 4 ##
+        # Test troubleshooting too many cpu cores
+        # Automatically reduce cpu cores
+        label = 'test'
+        level_of_theory = 'DLPNO ccsd(T)'
+        server = 'server1'
+        job_type = 'sp'
+        software = 'orca'
+        fine = True
+        memory_gb = 16
+        cpu_cores = 16
+        num_heavy_atoms = 1
+        ess_trsh_methods = ['cpu']
+        path = os.path.join(self.base_path['orca'], 'orca_too_many_cores.log')
+        status, keywords, error, line = trsh.determine_ess_status(output_path=path, species_label='TS', job_type='sp',
+                                                                  software=software)
+        job_status = {'keywords': keywords, 'error': error}
+        output_errors, ess_trsh_methods, remove_checkfile, level_of_theory, software, job_type, fine, trsh_keyword, \
+        memory, shift, cpu_cores, couldnt_trsh = trsh.trsh_ess_job(label, level_of_theory, server, job_status, job_type,
+                                                                   software, fine, memory_gb, num_heavy_atoms,
+                                                                   cpu_cores, ess_trsh_methods)
+        self.assertIn('cpu', ess_trsh_methods)
+        self.assertEqual(cpu_cores, 10)
 
     def test_trsh_negative_freq(self):
         """Test troubleshooting a negative frequency"""
