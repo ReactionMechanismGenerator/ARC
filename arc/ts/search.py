5bce29e8a58167e546dd24c72f6d250f21ae0984
Alon Grinberg Dana <alongd@mit.edu>
2020-01-09 22:21:57 -0500

    Added ts search

---
+++ arc/ts/search.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+A module for calling various TS guess searching algorithms:
+AutoTST, DEGSM, NEB (ASE, TeraChem), QST2/3 (Gaussian), KinBot, heuristics by RMG family
+"""
+
+
+
