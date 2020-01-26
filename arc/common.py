a8699ac37afda0fab9562680ce5aefc9c532c7c9
Alon Grinberg Dana <alongd@mit.edu>
2020-01-26 06:11:55 -0500

    Added get_single_bond_length to common

--- arc/common.py
+++ arc/common.py
@@ -872,3 +872,41 @@ def is_str_float(value):
         return True
     except ValueError:
         return False
+
+
+# a bond length dictionary of single bonds, Angstrom
+# https://sites.google.com/site/chempendix/bond-lengths
+# https://courses.lumenlearning.com/suny-potsdam-organicchemistry/chapter/1-3-basics-of-bonding/
+SINGLE_BOND_LENGTH = {'Br-Br': 2.29, 'Br-Cr': 1.94, 'Br-H': 1.41,
+                      'C-C': 1.54, 'C-Cl': 1.77, 'C-F': 1.35, 'C-H': 1.09, 'C-I': 2.13,
+                      'C-N': 1.47, 'C-O': 1.43, 'C-P': 1.87, 'C-S': 1.81, 'C-Si': 1.86,
+                      'Cl-Cl': 1.99, 'Cl-H': 1.27, 'Cl-N': 1.75, 'Cl-Si': 2.03, 'Cl-P': 2.03, 'Cl-S': 2.07,
+                      'F-F': 1.42, 'F-H': 0.92, 'F-P': 1.57, 'F-S': 1.56, 'F-Si': 1.56, 'F-Xe': 1.90,
+                      'H-H': 0.74, 'H-I': 1.61, 'H-N': 1.04, 'H-O': 0.96, 'H-P': 1.42, 'H-S': 1.34, 'H-Si': 1.48,
+                      'I-I': 2.66,
+                      'N-N': 1.45,
+                      'O-O': 1.48, 'O-P': 1.63, 'O-S': 1.58, 'O-Si': 1.66,
+                      'P-P': 2.21,
+                      'S-S': 2.05,
+                      'Si-Si': 2.35,
+                      }
+
+
+def get_single_bond_length(symbol1, symbol2):
+    """
+    Get the an approximate for a single bond length between two elements.
+
+    Args:
+        symbol1 (str): Symbol 1.
+        symbol2 (str): Symbol 2.
+
+    Returns:
+        float: The estimated single bond length in Angstrom.
+    """
+    bond1, bond2 = '-'.join([symbol1, symbol2]), '-'.join([symbol2, symbol1])
+    if bond1 in SINGLE_BOND_LENGTH.keys():
+        return SINGLE_BOND_LENGTH[bond1]
+    if bond2 in SINGLE_BOND_LENGTH.keys():
+        return SINGLE_BOND_LENGTH[bond2]
+    return 2.5
+
