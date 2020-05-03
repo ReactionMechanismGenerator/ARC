f6ae25016538a80fe515c6390e62358a047ad9ed
Xiaorui Dong <xiaorui@mit.edu>
2020-05-03 18:25:06 -0400

    Correct the docstring of `_get_additional_job_info`
    'download' is not accurate, and 'returns' are not indicated.

--- arc/job/job.py
+++ arc/job/job.py
@@ -1439,7 +1439,11 @@ end
 
     def _get_additional_job_info(self):
         """
-        Download the additional information of stdout and stderr from the server.
+        Get the additional information of stdout and stderr according to the 
+        queue software outputs.
+
+        Returns:
+            str: a str includes the info from stdout and stderr
         """
         lines1, lines2 = list(), list()
         content = ''
