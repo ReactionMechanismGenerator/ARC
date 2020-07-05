2f999ee067183155510ae1028705a1f0be23080f
Alon Grinberg Dana <alongd@mit.edu>
2020-07-05 00:22:33 -0400

    Minor: Create dirs when saving a YAML file only if path contains dirs

--- arc/common.py
+++ arc/common.py
@@ -368,7 +368,7 @@ def save_yaml_file(path: str,
     yaml.add_representer(str, string_representer)
     logger.debug('Creating a restart file...')
     content = yaml.dump(data=content)
-    if os.path.dirname(path) and not os.path.exists(os.path.dirname(path)):
+    if '/' in path and os.path.dirname(path) and not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
     with open(path, 'w') as f:
         f.write(content)
