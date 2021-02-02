f4011b097b2afda6a43a69ab719ca6c420d602cd
Kevin Spiekermann <kspieker@mit.edu>
2021-02-01 20:03:59 -0500

    f! try using `source activate` again

--- devtools/install_gcn_ci.sh
+++ devtools/install_gcn_ci.sh
@@ -3,6 +3,7 @@ pushd ..
 
 # clone the repo in the parent directory
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
+git checkout fdedb2ff2cb41f0b329fbc8b464593f4ccbe6c2a
 
 # create the environment
 cd TS-GCN/devtools
