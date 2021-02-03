4cb5def94ae59a1cef7d759c6b85c6fbf4fb04ce
Kevin Spiekermann <kspieker@mit.edu>
2021-02-02 21:55:19 -0500

    f! Temporarily checkout another branch from TS-GCN for debugging

--- devtools/install_gcn_ci.sh
+++ devtools/install_gcn_ci.sh
@@ -3,10 +3,13 @@ pushd ..
 
 # clone the repo in the parent directory
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
-git checkout fdedb2ff2cb41f0b329fbc8b464593f4ccbe6c2a
+cd TS-GCN
+git checkout update_devtools_GH_actions
+
 
 # create the environment
-cd TS-GCN/devtools
+# cd TS-GCN/devtools
+cd devtools
 bash create_env_cpu.sh
 
 # Restore original directory
