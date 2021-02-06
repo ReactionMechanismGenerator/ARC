f68469788a5a2703f6ae08bd57d784593112e9c4
Kevin Spiekermann <kspieker@mit.edu>
2021-02-06 17:55:19 -0500

    f! Use the master branch on TS-GCN
    no longer need debugging branch

--- devtools/install_gcn_ci.sh
+++ devtools/install_gcn_ci.sh
@@ -3,11 +3,9 @@ pushd ..
 
 # clone the repo in the parent directory
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
-cd TS-GCN
-git checkout update_devtools_GH_actions
 
 # create the environment
-cd devtools
+cd TS-GCN/devtools
 bash create_env_cpu.sh
 
 # Restore original directory
