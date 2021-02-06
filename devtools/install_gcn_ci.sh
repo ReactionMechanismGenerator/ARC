ca57386bd316896ce2dce388422de5a6c3187b85
Kevin Spiekermann <kspieker@mit.edu>
2021-02-06 15:29:17 -0500

    f! Try running make gcn-ci from base env, not arc_env
    Also use `source activate` on TS-GCN

--- devtools/install_gcn_ci.sh
+++ devtools/install_gcn_ci.sh
@@ -5,6 +5,8 @@ pushd ..
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
 cd TS-GCN
 git checkout update_devtools_GH_actions
+# use `source activate` on TS-GCN
+git checkout bba0dc66371c961005d17c00afa2411580e3bd26
 
 
 # create the environment
