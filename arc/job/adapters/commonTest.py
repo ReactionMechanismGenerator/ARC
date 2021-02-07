aa8b69b69e0ae27419f89a691889e9db8c1d9d24
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-07 15:15:56 +0200

    Tests: Added job adapters common tests

---
+++ arc/job/adapters/commonTest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+This module contains unit tests of the arc.job.adapters.common module
+"""
+
+import unittest
+
+import arc.job.adapters.common as common
+
+
+class TestJobCommon(unittest.TestCase):
+    """
+    Contains unit tests for the job.adapters.common module.
+    """
+
+    def test_which(self):
+        """Test the which() function"""
+        ans = common.which(command='python', return_bool=True, raise_error=False)
+        self.assertTrue(ans)
+
+        ans = common.which(command='python', return_bool=False, raise_error=False)
+        self.assertIn('arc_env/bin/python', ans)
+
+        ans = common.which(command='ARC.py', return_bool=True, raise_error=False)
+        self.assertTrue(ans)
+
+        ans = common.which(command='fake_command_1', return_bool=True, raise_error=False)
+        self.assertFalse(ans)
+
+        ans = common.which(command=['fake_command_1', 'ARC.py', 'python'], return_bool=False, raise_error=False)
+        self.assertEqual(ans, 'ARC.py')
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
