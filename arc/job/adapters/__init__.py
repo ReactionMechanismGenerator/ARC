ab6e694318123b42ee78b0bbd7d17263d0a82389
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-22 17:33:43 +0200

    Updated job adapters init

--- arc/job/adapters/__init__.py
+++ arc/job/adapters/__init__.py
@@ -1,2 +1,9 @@
+import arc.job.adapters.common
 import arc.job.adapters.gaussian
 import arc.job.adapters.molpro
+import arc.job.adapters.orca
+import arc.job.adapters.qchem
+import arc.job.adapters.terachem
+import arc.job.adapters.ts
+
+
