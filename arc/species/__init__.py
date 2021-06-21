dce1cf29d2fbcf932d6d074e8a08c5e993987ffa
Alon Grinberg Dana <alon@technion.ac.il>
2021-06-21 20:54:35 +0300

    Added TSGuess to species __init__.py

--- arc/species/__init__.py
+++ arc/species/__init__.py
@@ -2,4 +2,4 @@ import arc.species.conformers
 import arc.species.converter
 import arc.species.species
 import arc.species.xyz_to_2d
-from arc.species.species import ARCSpecies
+from arc.species.species import ARCSpecies, TSGuess
