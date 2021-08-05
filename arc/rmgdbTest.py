d5a579e0482af1f5ce0b10b9c4a7fc58e3a332a9
Alon Grinberg Dana <alon@technion.ac.il>
2021-08-05 15:19:48 +0300

    Tests: rmgdb.get_family()

--- arc/rmgdbTest.py
+++ arc/rmgdbTest.py
@@ -7,6 +7,7 @@ This module contains unit tests for the arc.rmgdb module
 
 import unittest
 
+from rmgpy.data.kinetics.family import KineticsFamily
 from rmgpy.data.kinetics.library import LibraryReaction
 from rmgpy.reaction import Reaction
 from rmgpy.species import Species
@@ -62,6 +63,28 @@ class TestRMGDB(unittest.TestCase):
                 self.assertAlmostEqual(rxn.kinetics.get_rate_coefficient(1000, 1e5), 38.2514795642, 7)
                 found_rxn = True
         self.assertTrue(found_rxn)
+
+    def test_get_family(self):
+        """Test the get_family() function"""
+        family_1 = rmgdb.get_family(rmgdb=self.rmgdb, label='ketoenol')
+        self.assertIsInstance(family_1, KineticsFamily)
+        self.assertEqual(family_1.label, 'ketoenol')
+        self.assertTrue(family_1.save_order)
+
+        family_2 = rmgdb.get_family(rmgdb=self.rmgdb, label='H_Abstraction')
+        self.assertIsInstance(family_2, KineticsFamily)
+        self.assertEqual(family_2.label, 'H_Abstraction')
+        self.assertTrue(family_2.save_order)
+
+        family_3 = rmgdb.get_family(rmgdb=self.rmgdb, label='intra_H_migration')
+        self.assertIsInstance(family_3, KineticsFamily)
+        self.assertEqual(family_3.label, 'intra_H_migration')
+        self.assertTrue(family_3.save_order)
+
+        family_4 = rmgdb.get_family(rmgdb=self.rmgdb, label='Intra_H_Migration')  # test wrong capitalization still works
+        self.assertIsInstance(family_4, KineticsFamily)
+        self.assertEqual(family_4.label, 'intra_H_migration')  # returns the correct capitalization
+        self.assertTrue(family_4.save_order)
 
 
 if __name__ == '__main__':
