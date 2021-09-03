08b7cf34173ff5b05a09d9a7114e1a59bdcc0604
Alon Grinberg Dana <alon@technion.ac.il>
2021-09-03 22:29:17 +0300

    Modifications to Makefile and additions of bash installation scripts
    To facilitate automatic installation of all necessary external packages, either individually or collectively.

---
+++ devtools/install_autotst.sh
@@ -0,0 +1,29 @@
+# temporarily change directory to install software, and move one directory up in the tree
+pushd .
+cd ../..
+
+# clone the repo in the parent directory
+echo "Cloning/Updating AutoTST..."
+git clone https://github.com/ReactionMechanismGenerator/AutoTST
+cd AutoTST || exit
+git fetch origin
+git checkout master
+git pull origin master
+
+# Add to PYTHONPATH
+echo "Adding AutoTST to PYTHONPATH..."
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+echo 'export PYTHONPATH=$PYTHONPATH:'"$(pwd)" >> ~/.bashrc
+echo $PYTHONPATH
+
+# create the environment
+echo "Creating the AutoTST environment..."
+conda deactivate
+conda env create -f environment.yml -y
+conda activate tst_env
+conda install -c anaconda yaml -y
+conda deactivate
+
+# Restore the original directory
+echo "Done installing AutoTST."
+popd || exit
--- devtools/install_gcn.sh
+++ devtools/install_gcn.sh
@@ -1,12 +1,26 @@
 # temporarily change directory to install software
-pushd ..
+pushd .
+cd ../..
 
-# clone the repo in the parent directory
+# clone the repo in the parent directory and update it
+echo "Cloning/Updating GCN..."
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
+cd TS-GCN || exit
+git fetch origin
+git checkout master
+git pull origin master
+
+# Add to PYTHONPATH
+echo "Adding GCN to PYTHONPATH..."
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+echo 'export PYTHONPATH=$PYTHONPATH:'"$(pwd)" >> ~/.bashrc
+echo $PYTHONPATH
 
 # create the environment
-cd TS-GCN || exit
+echo "Creating the GCN environment..."
+conda deactivate
 make conda_env
 
-# Restore original directory
+# Restore the original directory
+echo "Done installing GCN."
 popd || exit
