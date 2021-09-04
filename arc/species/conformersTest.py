e22fa7e3ed5f6f35cb36a265846414c55dd60733
Alon Grinberg Dana <alon@technion.ac.il>
2021-09-04 13:49:14 +0300

    Tests: Updated conformers tests due to OB call modification

--- arc/species/conformersTest.py
+++ arc/species/conformersTest.py
@@ -568,9 +568,12 @@ H       0.68104300    0.74807180    0.61546062""")]
                                       H         -0.78712        0.56391        0.71847
                                       O          1.39175        1.59510        0.11494""")
         spc = ARCSpecies(label='CCO[O]', smiles='CCO[O]', xyz=xyz)
-        rd_mol = conformers.embed_rdkit(label='', mol=spc.mol, num_confs=2, xyz=xyz)
-        xyzs, energies = conformers.openbabel_force_field_on_rdkit_conformers(label='', rd_mol=rd_mol,
-                                                                              force_field='MMFF94s',)
+        xyzs, energies = conformers.openbabel_force_field_on_rdkit_conformers(label='',
+                                                                              mol=spc.mol,
+                                                                              num_confs=2,
+                                                                              xyz=xyz,
+                                                                              force_field='MMFF94s',
+                                                                              )
         expected_xyzs = [{'symbols': ('C', 'C', 'H', 'H', 'H', 'O', 'H', 'H', 'O'),
                           'isotopes': (12, 12, 1, 1, 1, 16, 1, 1, 16),
                           'coords': ((-0.92004, 0.17591, -0.00274),
