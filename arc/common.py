ed1b1d6537a7fdf121b5201887551a039f34971c
Alon Grinberg Dana <alon@technion.ac.il>
2021-09-04 14:27:22 +0300

    Organize environment names and paths
    Relocated TS_GCN_PYTHON from common to settings
    Added AutoTST PYTHONPATHs to settings
    Added ARC_PYTHON path to settings
    
    ARC_PYTHON is used for executing runopenbabel.py within the arc_env
    
    Get the $HOME path even if it's undefined
    The os.getenv("HOME") expression returned None on ATLAS, but the os.path.expanduser("~") expression does return the correct $HOME path. Added as backup.

--- arc/common.py
+++ arc/common.py
@@ -46,13 +46,6 @@ RMG_DATABASE_PATH = os.path.abspath(os.path.dirname(rmgpy.settings['database.dir
 
 VERSION = '1.1.0'
 
-# define default values for using the optional GCN to predict TS guesses
-# default assumption is that TS-GCN is installed in the same parent folder as the ARC repository
-TS_GCN_PATH = os.path.join(os.path.dirname(ARC_PATH), 'TS-GCN')
-# default environment name for this repo is `ts_gcn`
-TS_GCN_PYTHON = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(sys.executable))),
-                             'ts_gcn', 'bin', 'python')
-
 
 default_job_types, servers = settings['default_job_types'], settings['servers']
 
