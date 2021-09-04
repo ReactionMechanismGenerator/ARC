b21720db06c10988222b189db5839dc387ad5ec1
Alon Grinberg Dana <alon@technion.ac.il>
2021-09-04 23:16:00 +0300

    Implement the mol <-> dict conversion is species
    Also added another exception when generating resonance structures

--- arc/species/species.py
+++ arc/species/species.py
@@ -12,8 +12,7 @@ import rmgpy.molecule.element as elements
 from arkane.common import ArkaneSpecies, symbol_by_number
 from arkane.statmech import is_linear
 from rmgpy.exceptions import AtomTypeError, ILPSolutionError, InvalidAdjacencyListError, ResonanceError
-from rmgpy.molecule.element import Element
-from rmgpy.molecule.molecule import Atom, Bond, Molecule
+from rmgpy.molecule.molecule import Atom, Molecule
 from rmgpy.molecule.resonance import generate_kekule_structure
 from rmgpy.reaction import Reaction
 from rmgpy.species import Species
@@ -25,6 +24,8 @@ from arc.common import (convert_list_index_0_to_1,
                         determine_top_group_indices,
                         get_logger,
                         get_single_bond_length,
+                        rmg_mol_from_dict_repr,
+                        rmg_mol_to_dict_repr,
                         timedelta_from_str,
                         )
 from arc.exceptions import InputError, RotorError, SpeciesError, TSError
@@ -638,24 +639,7 @@ class ARCSpecies(object):
         if self.bond_corrections is not None:
             species_dict['bond_corrections'] = self.bond_corrections
         if self.mol is not None:
-            species_dict['mol'] = {'atoms': [{'element': {'number': atom.element.number,
-                                                          'symbol':  atom.element.symbol,
-                                                          'name':  atom.element.name,
-                                                          'mass':  atom.element.mass,
-                                                          'isotope':  atom.element.isotope,
-                                                          },
-                                              'radical_electrons': atom.radical_electrons,
-                                              'charge': atom.charge,
-                                              'label': atom.label,
-                                              'lone_pairs': atom.lone_pairs,
-                                              'id': atom.id,
-                                              'props': atom.props,
-                                              'edges': {atom_2.id: bond.order
-                                                        for atom_2, bond in atom.edges.items()},
-                                              } for atom in self.mol.atoms],
-                                   'multiplicity': self.mol.multiplicity,
-                                   'props': self.mol.props,
-                                   }
+            species_dict['mol'] = rmg_mol_to_dict_repr(self.mol)
         if self.initial_xyz is not None:
             species_dict['initial_xyz'] = xyz_to_str(self.initial_xyz)
         if self.final_xyz is not None:
@@ -784,31 +768,7 @@ class ARCSpecies(object):
                 except (ValueError, AtomTypeError, InvalidAdjacencyListError) as e:
                     logger.error(f"Could not read RMG adjacency list {species_dict['mol']}.\nGot:\n{e}")
             else:
-                self.mol = Molecule(multiplicity=species_dict['mol']['multiplicity'],
-                                    props=species_dict['mol']['props'])
-                atoms = [Atom(element=Element(number=atom_dict['element']['number'],
-                                              symbol=atom_dict['element']['symbol'],
-                                              name=atom_dict['element']['name'],
-                                              mass=atom_dict['element']['mass'],
-                                              isotope=atom_dict['element']['isotope'],
-                                              ),
-                              radical_electrons=atom_dict['radical_electrons'],
-                              charge=atom_dict['charge'],
-                              lone_pairs=atom_dict['lone_pairs'],
-                              id=atom_dict['id'],
-                              props=atom_dict['props'],
-                              ) for atom_dict in species_dict['mol']['atoms']]
-                for i, atom in enumerate(atoms):
-                    edges = dict()
-                    for atom_2_id, bond_order in species_dict['mol']['atoms'][i]['edges'].items():
-                        for atom_2 in atoms:
-                            if atom_2.id == atom_2_id:
-                                break
-                        edges[atom_2] = Bond(atom, atom_2, bond_order)
-                    atom.edges = edges
-                self.mol.atoms = atoms
-                self.mol.update_atomtypes(raise_exception=False)
-                self.mol.identify_ring_membership()
+                self.mol = rmg_mol_from_dict_repr(species_dict['mol'], is_ts=self.is_ts)
         else:
             self.mol = None
         smiles = species_dict['smiles'] if 'smiles' in species_dict else None
