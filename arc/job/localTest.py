495876010127a01d1b2c9346af815db725de6ec7
Alon Grinberg Dana <alon@technion.ac.il>
2021-09-19 14:36:23 +0300

    Tests: job local parse_running_jobs_ids()
    f! local tests

--- arc/job/localTest.py
+++ arc/job/localTest.py
@@ -50,6 +50,29 @@ class TestLocal(unittest.TestCase):
         self.assertTrue(os.path.isfile(path2))
         shutil.rmtree(os.path.join(ARC_PATH, 'scratch'), ignore_errors=True)
 
+    def test_parse_running_jobs_ids(self):
+        """Test the parse_running_jobs_ids() function"""
+        # Slurm:
+        stdout = ['             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)',
+                  '          10990729    normal     a207   alongd PD       0:00      1 (None)',
+                  '          10990728   xeon-p8  xa1001d   alongd  R       0:05      1 d-19-14-2',
+                  ]
+        running_job_ids = local.parse_running_jobs_ids(stdout, cluster_soft='slurm')
+        self.assertEqual(running_job_ids, ['10990729', '10990728'])
+
+        # HTCondor:
+        stdout = ['11224.0 R 8 6759 a2495 7',
+                  '11225.0 R 8 6759 a2496 6',
+                  '11226.0 R 8 6759 a2497 7',
+                  '11227.0 R 8 6759 a2498 7',
+                  '11228.0 R 8 6759 a2499 7',
+                  '11229.0 P 8 6759 a2500 14',
+                  '11230.0 P 8 6759 a2501 13',
+                  '11231.0 P 8 6759 a2502 13',
+                  ]
+        running_job_ids = local.parse_running_jobs_ids(stdout, cluster_soft='htcondor')
+        self.assertEqual(running_job_ids, ['11224', '11225', '11226', '11227', '11228', '11229', '11230', '11231'])
+
 
 if __name__ == '__main__':
     unittest.main(testRunner=unittest.TextTestRunner(verbosity=2))
