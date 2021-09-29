cfee68b8c22f984959c86caed1284c1ec09384a8
Alon Grinberg Dana <alon@technion.ac.il>
2021-09-29 11:09:57 +0300

    Added the xyz_to_smiles module
    f! xyz_to_smiles
    
    
    f! xyz_to_smiles

--- arc/species/__init__.py
+++ arc/species/__init__.py
@@ -2,4 +2,5 @@ import arc.species.conformers
 import arc.species.converter
 import arc.species.species
 import arc.species.xyz_to_2d
+import arc.species.xyz_to_smiles
 from arc.species.species import ARCSpecies, TSGuess
