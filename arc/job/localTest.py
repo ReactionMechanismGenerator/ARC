9f969c11ce6eb2052a437088768a48f0e403b1af
Alon Grinberg Dana <alon@technion.ac.il>
2022-05-04 14:37:50 +0300

    Tests: Added tests to localTest
    - determine_job_id()
    - parse_running_jobs_ids

--- arc/job/localTest.py
+++ arc/job/localTest.py
@@ -26,10 +26,31 @@ class TestLocal(unittest.TestCase):
         self.assertIsInstance(out1, tuple)
         self.assertIsInstance(out1[0], list)
         self.assertIsInstance(out1[0][0], str)
-        self.assertEqual(out1[1], '')
-        self.assertIn('arc', out1[0])
-        self.assertIn('ARC.py', out1[0])
-        self.assertIn('environment.yml', out1[0])
+        self.assertEqual(out1[1], [])
+        if 'arc' in out1[0]:
+            # Running from root
+            self.assertIn('ARC.py', out1[0])
+            self.assertIn('environment.yml', out1[0])
+        else:
+            # Running directly
+            self.assertIn('adapter.py', out1[0])
+            self.assertIn('ssh.py', out1[0])
+
+    def test_determine_job_id(self):
+        """Test determining a job ID from the stdout of a job submission command."""
+        # Slurm
+        stdout_1 = ['Submitted batch job 17670585']
+        job_id = local._determine_job_id(stdout_1, cluster_soft='slurm')
+        self.assertEqual(job_id, '17670585')
+
+        # HTCondor
+        stdout_2 = ['Submitting job(s).', '1 job(s) submitted to cluster 5263.']
+        job_id = local._determine_job_id(stdout_2)
+        self.assertEqual(job_id, '5263')
+
+        # Wrong server name
+        with self.assertRaises(ValueError):
+            local._determine_job_id(stdout_2, cluster_soft='wrong')
 
     def test_get_last_modified_time(self):
         """Test the get_last_modified_time() function"""
@@ -51,6 +72,29 @@ class TestLocal(unittest.TestCase):
         self.assertFalse(os.path.isfile(path1))
         self.assertTrue(os.path.isfile(path2))
         shutil.rmtree(os.path.join(ARC_PATH, 'scratch'), ignore_errors=True)
+
+    def test_parse_running_jobs_ids(self):
+        """Test the parse_running_jobs_ids() function"""
+        # Slurm:
+        stdout = ['             JOBID PARTITION     NAME     USER ST       TIME  NODES NODELIST(REASON)',
+                  '          10990729    normal     a207   alongd PD       0:00      1 (None)',
+                  '          10990728   xeon-p8  xa1001d   alongd  R       0:05      1 d-19-14-2',
+                  ]
+        running_job_ids = local.parse_running_jobs_ids(stdout, cluster_soft='slurm')
+        self.assertEqual(running_job_ids, ['10990729', '10990728'])
+
+        # HTCondor:
+        stdout = ['11224.0 R 8 6759 a2495 7',
+                  '11225.0 R 8 6759 a2496 6',
+                  '11226.0 R 8 6759 a2497 7',
+                  '11227.0 R 8 6759 a2498 7',
+                  '11228.0 R 8 6759 a2499 7',
+                  '11229.0 P 8 6759 a2500 14',
+                  '11230.0 P 8 6759 a2501 13',
+                  '11231.0 P 8 6759 a2502 13',
+                  ]
+        running_job_ids = local.parse_running_jobs_ids(stdout, cluster_soft='htcondor')
+        self.assertEqual(running_job_ids, ['11224', '11225', '11226', '11227', '11228', '11229', '11230', '11231'])
 
 
 if __name__ == '__main__':
