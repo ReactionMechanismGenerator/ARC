27e6d74f8fe4fe9a649fdc2f32710c25b7297533
Alon Grinberg Dana <alon@technion.ac.il>
2022-05-18 16:10:10 +0300

    Modifications to Makefile and additions of bash installation scripts
    To facilitate automatic installation of all necessary external packages, either individually or collectively.

---
+++ devtools/install_all.sh
@@ -0,0 +1,15 @@
+# Note: this script assumes the user is running it from the root ARC folder.
+
+# Temporarily change directory to install software (don't use "|| exit" here)
+pushd .
+
+# install dependencies
+bash devtools/install_autotst.sh
+bash devtools/install_gcn_cpu.sh
+bash devtools/install_kinbot.sh
+echo "Done installing external repos."
+source ~/.bashrc
+echo $PYTHONPATH
+
+# Restore original directory
+popd || exit
--- devtools/install_gcn_cpu.sh
+++ devtools/install_gcn_cpu.sh
@@ -1,13 +1,27 @@
 # temporarily change directory to install software
-pushd ..
+pushd .
+cd ..
 
-# clone the repo in the parent directory
+# clone the repo in the parent directory and update it
+echo "Cloning/Updating GCN..."
 git clone https://github.com/ReactionMechanismGenerator/TS-GCN
+cd TS-GCN || exit
+git fetch origin
+git checkout main
+git pull origin main
+
+# Add to PYTHONPATH
+echo "Adding GCN to PYTHONPATH"
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+echo 'export PYTHONPATH=$PYTHONPATH:'"$(pwd)" >> ~/.bashrc
+echo $PYTHONPATH
 
 # create the environment
-cd TS-GCN || exit
-cd devtools || exit
-bash create_env_cpu.sh
+echo "Creating the GCN-cpu environment..."
+source ~/.bashrc
+bash devtools/create_env_cpu.sh
 
-# Restore original directory
+# Restore the original directory
+cd ../ARC || exit
+echo "Done installing GCN-cpu."
 popd || exit
