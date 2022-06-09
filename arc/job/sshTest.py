2cf2c47b1fe6450ff7598768401a16992dd37c2a
Alon Grinberg Dana <alon@technion.ac.il>
2022-06-09 05:17:12 +0300

    Tests: check_job_status_in_stdout in ssh

--- arc/job/sshTest.py
+++ arc/job/sshTest.py
@@ -23,24 +23,61 @@ class TestSSH(unittest.TestCase):
  582682 0.45451 a9654      alongd       e     04/17/2019 16:22:14 long5@node93.cluster              48
  588334 0.45451 pf1005a    alongd       r     05/07/2019 16:24:31 long3@node67.cluster              48
  588345 0.45451 a14121     alongd       r     05/08/2019 02:11:42 long3@node69.cluster              48    """
-        status1 = ssh.check_job_status_in_stdout(job_id=588345, stdout=stdout_1, server='server1')
-        self.assertEqual(status1, 'running')
-        status2 = ssh.check_job_status_in_stdout(job_id=582682, stdout=stdout_1, server='server1')
-        self.assertEqual(status2, 'errored')
-        status3 = ssh.check_job_status_in_stdout(job_id=582600, stdout=stdout_1, server='server1')
-        self.assertEqual(status3, 'done')
+        status = ssh.check_job_status_in_stdout(job_id=588345, stdout=stdout_1, server='server1')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=582682, stdout=stdout_1, server='server1')
+        self.assertEqual(status, 'errored')
+        status = ssh.check_job_status_in_stdout(job_id=582600, stdout=stdout_1, server='server1')
+        self.assertEqual(status, 'done')
+
+        # Slurm
+        stdout_2 = ['14428     debug xq1371m2   user_name  R 50-04:04:46      1 node06',
+                    '14529     debug xq1371m2   user_name  F 50-04:04:46      1 node06',
+                    '14429     debug xq1371m2   user_name PD 50-04:04:46      1 node06']
+        status = ssh.check_job_status_in_stdout(job_id=14428, stdout=stdout_2, cluster_soft='Slurm')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id='14428', stdout=stdout_2, cluster_soft='Slurm')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id='14429', stdout=stdout_2, cluster_soft='Slurm')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=14430, stdout=stdout_2, cluster_soft='Slurm')
+        self.assertEqual(status, 'done')
+        status = ssh.check_job_status_in_stdout(job_id=14529, stdout=stdout_2, cluster_soft='Slurm')
+        self.assertEqual(status, 'errored')
 
         # HTCondor
-        stdout_2 = ['5231.0 R 10 7885 a20596 130',
+        stdout_3 = ['5231.0 R 10 7885 a20596 130',
                     '5232.0 R 10 7885 a20597 130',
                     '5233.0 R 10 7885 a20598 130',
                     '5241.0 P 10 7885 a20616 0']
-        status1 = ssh.check_job_status_in_stdout(job_id=5231, stdout=stdout_2, server='local')
-        self.assertEqual(status1, 'running')
-        status1 = ssh.check_job_status_in_stdout(job_id=5241, stdout=stdout_2, server='local')
-        self.assertEqual(status1, 'running')
-        status1 = ssh.check_job_status_in_stdout(job_id=4000, stdout=stdout_2, server='local')
-        self.assertEqual(status1, 'done')
+        status = ssh.check_job_status_in_stdout(job_id=5231, stdout=stdout_3, server='local')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=5241, stdout=stdout_3, cluster_soft='HTCondor')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=4000, stdout=stdout_3, server='local')
+        self.assertEqual(status, 'done')
+
+        # Cobalt
+        stdout_4 = ['JobID   User       WallTime  Nodes  State     Location',
+                    '========================================================',
+                    '602991  user_name  00:01:00  1      queued    None',
+                    '602992  user_name  00:01:00  1      starting  0',
+                    '602993  user_name  00:01:00  1      running   0',
+                    '602994  user_name  00:01:00  1      exiting   0',
+                    '602995  user_name  00:01:00  1      errored   0',
+                    '602996  user_name  00:01:00  1      killing   0']
+        status = ssh.check_job_status_in_stdout(job_id=602991, stdout=stdout_4, cluster_soft='Cobalt')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=602992, stdout=stdout_4, cluster_soft='Cobalt')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=602993, stdout=stdout_4, cluster_soft='Cobalt')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=602994, stdout=stdout_4, cluster_soft='Cobalt')
+        self.assertEqual(status, 'running')
+        status = ssh.check_job_status_in_stdout(job_id=602995, stdout=stdout_4, cluster_soft='Cobalt')
+        self.assertEqual(status, 'errored')
+        status = ssh.check_job_status_in_stdout(job_id=602996, stdout=stdout_4, cluster_soft='Cobalt')
+        self.assertEqual(status, 'running')
 
 
 if __name__ == '__main__':
