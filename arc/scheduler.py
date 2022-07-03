581aef9961387102df20b3d04d9dcfea90454777
Alon Grinberg Dana <alon@technion.ac.il>
2022-07-03 13:49:20 +0300

    d! s

--- arc/scheduler.py
+++ arc/scheduler.py
@@ -2394,6 +2394,7 @@ class Scheduler(object):
                     print([(spc_label, species_has_sp(output_dict), species_has_freq(output_dict),
                            self.species_dict[label].yml_path is not None)
                            for spc_label, output_dict in self.output.items() if spc_label in labels])
+                    print(f'spc {spc_label} has yml path {self.species_dict[spc_label].yml_path}')
             if label in labels and not rxn.ts_species.ts_checks['E0'] \
                     and all([(species_has_sp(output_dict) and species_has_freq(output_dict))
                              or self.species_dict[label].yml_path is not None
