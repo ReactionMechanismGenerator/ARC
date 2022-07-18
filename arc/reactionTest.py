142efe18a408bb695e5faf4d3b99c85d0df1a8db
Alon Grinberg Dana <alon@technion.ac.il>
2022-07-18 07:46:07 +0300

    Tests: Reaction.get_element_masses()

--- arc/reactionTest.py
+++ arc/reactionTest.py
@@ -12,7 +12,7 @@ from rmgpy.reaction import Reaction
 from rmgpy.species import Species
 
 import arc.rmgdb as rmgdb
-from arc.common import ARC_PATH
+from arc.common import ARC_PATH, almost_equal_lists
 from arc.exceptions import ReactionError
 from arc.reaction import ARCReaction, remove_dup_species
 from arc.species import ARCSpecies
@@ -1676,6 +1676,18 @@ H       1.12853146   -0.86793870    0.06973060"""
                             )
         self.assertEqual(rxn_2.reactants, ['NO2', '[O-][NH3p]'])
         self.assertEqual(rxn_2.products, ['H2NO', 'HONO'])
+
+    def test_get_element_masses(self):
+        """Test the get_element_masses() method."""
+        self.assertTrue(almost_equal_lists(self.rxn1.get_element_mass(),
+                                           [12.0, 1.00782503224, 1.00782503224, 1.00782503224,
+                                            1.00782503224, 15.99491461957, 1.00782503224]))
+        self.assertTrue(almost_equal_lists(self.rxn2.get_element_mass(),
+                                           [12.0, 12.0, 1.00782503224, 1.00782503224, 1.00782503224, 1.00782503224,
+                                            1.00782503224, 15.99491461957, 1.00782503224]))
+        self.assertTrue(almost_equal_lists(self.rxn3.get_element_mass(),
+                                           [12.0, 12.0, 14.00307400443, 1.00782503224, 1.00782503224, 1.00782503224,
+                                            1.00782503224, 1.00782503224, 1.00782503224]))
 
 
 def check_atom_map(rxn: ARCReaction) -> bool:
