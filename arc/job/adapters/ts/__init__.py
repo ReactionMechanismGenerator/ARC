f480390671d5749107427c0dd874a31fb34a6f5d
Alon Grinberg Dana <alon@technion.ac.il>
2022-07-23 01:03:53 +0300

    f! ts gcn init

--- arc/job/adapters/ts/__init__.py
+++ arc/job/adapters/ts/__init__.py
@@ -2,3 +2,5 @@ import arc.job.adapters.ts.autotst_ts
 import arc.job.adapters.ts.gcn_ts
 import arc.job.adapters.ts.kinbot_ts
 import arc.job.adapters.ts.heuristics
+
+from arc.job.adapters.ts.gcn_ts import run_subprocess_locally, process_tsg
