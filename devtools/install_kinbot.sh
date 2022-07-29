05c47105175c16d2b0fc11c3d48b1f6ade40c85d
Alon Grinberg Dana <alon@technion.ac.il>
2022-07-29 17:10:40 +0300

    Added an xTB, Sella, and TorchANI installation scripts
    Also added `source conda.sh` to other installation scripts

--- devtools/install_kinbot.sh
+++ devtools/install_kinbot.sh
@@ -1,3 +1,7 @@
+# Properly configure the shell to use 'conda activate'.
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+
 # temporarily change directory to install software, and move one directory up in the tree
 pushd .
 cd ..
