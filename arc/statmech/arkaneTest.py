fba4a4cc314e3c638a02fc2f68c3d84bd6e2858e
Alon Grinberg Dana <alon@technion.ac.il>
2022-07-31 11:59:31 +0300

    Tests: Run Arkane statmech and get thermo data

--- arc/statmech/arkaneTest.py
+++ arc/statmech/arkaneTest.py
@@ -9,7 +9,7 @@ import os
 import shutil
 import unittest
 
-import arc.common as common
+from arc.common import ARC_PATH
 from arc.level import Level
 from arc.reaction import ARCReaction, ARCSpecies
 from arc.statmech.arkane import ArkaneAdapter
@@ -24,8 +24,12 @@ class TestArkaneAdapter(unittest.TestCase):
         """
         A method that is run before all unit tests in this class.
         """
-        path_1 = os.path.join(common.ARC_PATH, 'arc', 'testing', 'statmech', 'arkane_1')
-        os.makedirs(path_1)
+        path_1 = os.path.join(ARC_PATH, 'arc', 'testing', 'arkane_tests_delete', 'arkane_1')
+        path_2 = os.path.join(ARC_PATH, 'arc', 'testing', 'arkane_tests_delete', 'arkane_2')
+        path_3 = os.path.join(ARC_PATH, 'arc', 'testing', 'arkane_tests_delete', 'arkane_3')
+        for path in [path_1, path_2, path_3]:
+            if not os.path.isdir(path):
+                os.makedirs(path)
         rxn_1 = ARCReaction(r_species=[ARCSpecies(label='CH3NH', smiles='C[NH]')],
                             p_species=[ARCSpecies(label='CH2NH2', smiles='[CH2]N')])
         rxn_1.ts_species = ARCSpecies(label='TS1', is_ts=True, xyz="""C      -0.68121000   -0.03232800    0.00786900
@@ -35,7 +39,22 @@ class TestArkaneAdapter(unittest.TestCase):
                                                                       N       0.74713700    0.12694800   -0.09842700
                                                                       H       1.16150600   -0.79827600    0.01973500""")
         cls.arkane_1 = ArkaneAdapter(output_directory=path_1, output_dict=dict(), bac_type=None, reaction=rxn_1)
-        cls.arkane_2 = ArkaneAdapter(output_directory=path_1, output_dict=dict(), bac_type=None, species=rxn_1.r_species[0])
+        cls.arkane_2 = ArkaneAdapter(output_directory=path_2, output_dict=dict(), bac_type=None,
+                                     species=rxn_1.r_species[0])
+        cls.ic3h7 = ARCSpecies(label='iC3H7', smiles='C[CH]C')
+        cls.ic3h7.e_elect = 150.1
+        opt_path = os.path.join(ARC_PATH, 'arc', 'testing', 'statmech', 'iC3H7_xtb', 'opt_a1596', 'output.out')
+        freq_path = os.path.join(ARC_PATH, 'arc', 'testing', 'statmech', 'iC3H7_xtb', 'freq_a1597', 'output.out')
+        cls.arkane_3 = ArkaneAdapter(output_directory=path_3,
+                                     output_dict={'iC3H7': {'paths': {'freq': freq_path,
+                                                                      'sp': opt_path,
+                                                                      'opt': opt_path,
+                                                                      'composite': '',
+                                                                      }}},
+                                     bac_type=None,
+                                     species=cls.ic3h7,
+                                     sp_level=Level('gfn2'),
+                                     )
 
     def test_arkane_has_en_corr(self):
         """Test the arkane_has_en_corr() function"""
@@ -55,12 +74,19 @@ class TestArkaneAdapter(unittest.TestCase):
         self.arkane_2.sp_level = Level(method='fake', basis='basis')
         self.assertFalse(self.arkane_2.arkane_has_en_corr())
 
+    def test_run_statmech_using_molecular_properties(self):
+        """Test running statmech using molecular properties."""
+        self.arkane_3.compute_thermo()
+        self.assertTrue(os.path.isfile(os.path.join(ARC_PATH, 'arc', 'testing', 'arkane_tests_delete', 'arkane_3',
+                                                    'Species', 'iC3H7', 'thermo_properties_plot.pdf')))
+        self.assertAlmostEqual(self.ic3h7.thermo.E0.value_si, 25996536.997005656)
+
     @classmethod
     def tearDownClass(cls):
         """
         A function that is run ONCE after all unit tests in this class.
         """
-        shutil.rmtree(os.path.join(common.ARC_PATH, 'arc', 'testing', 'statmech'), ignore_errors=True)
+        shutil.rmtree(os.path.join(ARC_PATH, 'arc', 'testing', 'arkane_tests_delete'), ignore_errors=True)
 
 
 if __name__ == '__main__':
