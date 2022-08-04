17d84b73e28b74fb8acd5abda68c97227e14519a
Alon Grinberg Dana <alon@technion.ac.il>
2022-08-04 21:48:52 +0300

    f! main test

--- arc/mainTest.py
+++ arc/mainTest.py
@@ -103,7 +103,7 @@ class TestARC(unittest.TestCase):
                                           'qchem': ['server1'],
                                           'terachem': ['server1'],
                                           'xtb': ['local'],
-                                          'xtb_gsm': ['server1'],
+                                          'xtb_gsm': ['local'],
                                           },
                          'freq_level': {'basis': '6-311+g(3df,2p)',
                                         'method': 'b3lyp',
