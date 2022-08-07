ee194a7e2df5de6723b257f5be157c26a5a84adb
Alon Grinberg Dana <alon@technion.ac.il>
2022-08-07 09:39:01 +0300

    Added xTB and Sella installation scripts
    Also added `source conda.sh` to other installation

--- devtools/install_all.sh
+++ devtools/install_all.sh
@@ -7,6 +7,8 @@ pushd .
 bash devtools/install_autotst.sh
 bash devtools/install_gcn_cpu.sh
 bash devtools/install_kinbot.sh
+bash devtools/install_xtb.sh
+bash devtools/install_sella.sh
 echo "Done installing external repos."
 source ~/.bashrc
 echo $PYTHONPATH
