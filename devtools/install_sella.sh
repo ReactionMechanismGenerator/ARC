e7a0860ee3a72fb5b01db88f3bb94d3eca11aebe
Calvin <calvinpieters@gmail.com>
2023-01-08 15:41:18 +0200

    Optimised code and LD_LIBRARY_PATH
    Optimised the bash script to now look for mamba/conda and if can't find either, will echo to the user they need a package manager and then will exit.
    
    Furthermore, setting the LD_LIBRARY_PATH to include sella_env/lib all the time causes an error with the command nano (segmentation fault). Therefore, the LD_LIBRARY_PATH will include sella_env/lib ONLY when the environment is activated

--- devtools/install_sella.sh
+++ devtools/install_sella.sh
@@ -2,15 +2,14 @@
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 
-#Check if mamba/conda is installed
-if [ -x "$(command -v mamba)" ]; then
-	echo "mamba is installed."
-	COMMAND_PKG=mamba
-elif [ -x "$(command -v conda)" ]; then
-	echo "conda is installed."
-	COMMAND_PKG=conda
+# Check if mamba or conda is installed
+if command -v mamba > /dev/null; then
+  COMMAND_PKG=mamba
+elif command -v conda > /dev/null; then
+  COMMAND_PKG=conda
 else
-    echo "mamba and conda are not installed. Please download and install mamba or conda - we strongly recommend mamba"
+  echo "mamba and conda are not installed. Please download and install mamba or conda - we strongly recommend mamba"
+  exit 1
 fi
 
 # clone the repo in the parent directory
@@ -25,6 +24,24 @@ $COMMAND_PKG  install -c conda-forge ase -y
 $COMMAND_PKG  install -c conda-forge ncurses
 $COMMAND_PKG  install -c anaconda pip -y
 $CONDA_BASE/envs/sella_env/bin/pip install sella
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'"$CONDA_BASE"'/envs/sella_env/lib' >> ~/.bashrc
+
+
+
+
+cd $CONDA_BASE/envs/sella_env
+mkdir -p ./etc/conda/activate.d
+mkdir -p ./etc/conda/deactivate.d
+touch ./etc/conda/activate.d/env_vars.sh
+touch ./etc/conda/deactivate.d/env_vars.sh
+
+#This sets up the LD_LIBRARY_PATH to include Sella_Env, but only when the environment is active
+echo 'export OLD_LD_LIBRARY_PATH=${LD_LIBRARY_PATH}' >> $CONDA_BASE/envs/sella_env/etc/conda/activate.d/env_vars.sh
+echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:'"$CONDA_BASE"'/envs/sella_env/lib' >> $CONDA_BASE/envs/sella_env/etc/conda/activate.d/env_vars.sh
+#This will reset the LD_LIBRARY_PATH back to the original LD_LIBRARY_PATH when the environment is deactivated
+echo 'export LD_LIBRARY_PATH=${OLD_LD_LIBRARY_PATH}' >> $CONDA_BASE/envs/sella_env/etc/conda/deactivate.d/env_vars.sh
+echo 'unset OLD_LD_LIBRARY_PATH' >> $CONDA_BASE/envs/sella_env/etc/conda/deactivate.d/env_vars.sh
+
+
+
 source ~/.bashrc
 echo "Done installing Sella."
