4bd14e3a42206212d19ddaaeb5d5ca19ffb0238b
Calvin Pieters <calvinpieters@gmail.com>
2023-02-08 17:08:48 +0200

    Added kinbot to init for adapters/ts

--- arc/job/adapters/ts/__init__.py
+++ arc/job/adapters/ts/__init__.py
@@ -3,3 +3,4 @@ import arc.job.adapters.ts.gcn_ts
 import arc.job.adapters.ts.heuristics
 import arc.job.adapters.ts.kinbot_ts
 import arc.job.adapters.ts.xtb_gsm
+import arc.job.adapters.ts.kinbot_adapter
