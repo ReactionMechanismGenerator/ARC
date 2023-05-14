4994b0d73682ba40aa20ca0e3e897b6dbeee89b7
Alon Grinberg Dana <alon@technion.ac.il>
2023-05-14 17:17:29 +0300

    fixup! molpro comment

--- arc/job/adapters/molpro.py
+++ arc/job/adapters/molpro.py
@@ -320,6 +320,15 @@ ${self.species[0].occ}wf,spin=${input_dict['spin']},charge=${input_dict['charge'
         Set the input_file_memory attribute.
         """
         # Molpro's memory is per cpu core and in MW (mega word; 1000 MW = 7.45 GB on a 64-bit machine)
+        # The conversion from mW to GB was done using this (https://deviceanalytics.com/words-to-bytes-converter/)
+        # specifying a 64-bit architecture.
+        #
+        # See also:
+        # https://www.molpro.net/pipermail/molpro-user/2010-April/003723.html
+        # In the link, they describe the conversion of 100,000,000 Words (100Mword) is equivalent to
+        # 800,000,000 bytes (800 mb).
+        # Formula - (100,000,000 [Words]/( 800,000,000 [Bytes] / (job mem in gb * 1000,000,000 [Bytes])))/ 1000,000 [Words -> MegaWords]
+        # The division by 1E6 is for converting into MWords
         self.input_file_memory = math.ceil(self.job_memory_gb / (7.45e-3 * self.cpu_cores))
         
     def execute_incore(self):
