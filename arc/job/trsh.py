a4051867ddfc1ecd2a5d5f1863f09fca72153c6d
Calvin Pieters <calvinpieters@gmail.com>
2023-06-15 11:10:48 +0300

    Moved the error for Qchem additional mem required

--- arc/job/trsh.py
+++ arc/job/trsh.py
@@ -363,12 +363,12 @@ def determine_ess_status(output_path: str,
                     # e.g.: `A further 246.03 Mwords of memory are needed for the triples to run.
                     # Increase memory to 996.31 Mwords.` (w/o the line break)
                     keywords = ['Memory']
+                    error = f'Additional memory required: {line.split()[2]} MW'
                     for line0 in reverse_lines:
                         if ' For full I/O' in line0 and 'increase memory by' in line0 and 'Mwords to' in line0:
                             memory_increase = re.findall(r"[\d.]+", line0)[0]
                             error = f"Additional memory required: {memory_increase} MW"
-                            break
-                    error = f'Additional memory required: {line.split()[2]} MW' 
+                            break                         
                     break
                 elif 'insufficient memory available - require' in line:
                     # e.g.: `insufficient memory available - require              228765625  have
