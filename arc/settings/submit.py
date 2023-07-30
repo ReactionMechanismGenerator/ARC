2bf315cbc10eadc3ef41f52f6e2505539f5f7b4b
Calvin Pieters <calvinpieters@gmail.com>
2023-07-30 11:46:14 +0300

    submit.py & settings.py: Updated for SLURM and AZURE

--- arc/settings/submit.py
+++ arc/settings/submit.py
@@ -132,7 +132,7 @@ touch final_time
 #SBATCH -p long
 #SBATCH -J {name}
 #SBATCH -N 1
-#SBATCH -n {cpus}
+#SBATCH --ntask-per-node={cpus}
 #SBATCH --time={t_max}
 #SBATCH --mem-per-cpu={memory}
 #SBATCH -o out.txt
@@ -158,7 +158,7 @@ cd $sdir
 
 cp "$SubmitDir/input.in" .
 
-molpro -n {cpus} -d $sdir input.in
+molpro -n {cpus} -t {cpus} -d $sdir input.in
 
 cp input.* "$SubmitDir/"
 cp geometry*.* "$SubmitDir/"
@@ -282,6 +282,33 @@ touch initial_time
 conda activate xtb_env
 
 ./gsm.orca
+
+touch final_time
+
+""",
+        'qchem': """#!/bin/bash -l
+#SBATCH -p long
+#SBATCH -J {name}
+#SBATCH -N 1
+#SBATCH -cpus-per-task={cpus}
+#SBATCH --time={t_max}
+#SBATCH --mem-per-cpu={memory}
+#SBATCH -o out.txt
+#SBATCH -e err.txt
+
+ . /opt/qchem/qchem_env.sh
+
+echo "============================================================"
+echo "Job ID : $SLURM_JOB_ID"
+echo "Job Name : $SLURM_JOB_NAME"
+echo "Starting on : $(date)"
+echo "Running on node : $SLURMD_NODENAME"
+echo "Current directory : $(pwd)"
+echo "============================================================"
+
+touch initial_time
+
+qchem -nt {cpus} input.in output.out
 
 touch final_time
 
@@ -456,7 +483,7 @@ hostname
 source /opt/qchem/qcenv.sh
 
 export QC=/opt/qchem
-export QCSCRATCH=/scratch/{un}/{name}
+export QCSCRATCH=$PWD
 export QCLOCALSCR=/scratch/{un}/{name}/qlscratch
 . $QC/qcenv.sh
 
@@ -855,6 +882,148 @@ touch final_time
 
     """,
     },
+        'azure': {
+            'qchem': """#!/bin/bash -l
+#SBATCH -p hpc,htc
+#SBATCH -J {name}
+#SBATCH --account={un}
+#SBATCH -N 1
+#SBATCH --cpus-per-task={cpus}
+#SBATCH --mem={memory}
+#SBATCH -o out.txt
+#SBATCH -e err.txt
+
+# Load QChem module
+module load easybuild/EasyBuild
+module load QChem-6.1
+
+# Get Current Directory
+export CWD="{pwd}"
+
+# Set up scratch directory
+export SCRATCH=/mnt/{un}/scratch/$SLURM_JOB_ID
+if [ -d $SCRATCH ]; then
+        sudo rm -vrf $SCRATCH
+fi
+sudo mkdir -p $SCRATCH
+
+# Change permissions
+sudo chmod 777 $SCRATCH
+export QCSCRATCH=$SCRATCH
+
+echo "============================================================"
+echo "Job ID : $SLURM_JOB_ID"
+echo "Job Name : $SLURM_JOB_NAME"
+echo "Starting on : $(date)"
+echo "Running on node : $SLURMD_NODENAME"
+echo "Current directory : {pwd}"
+echo "============================================================"
+
+
+# Due to a bug in the Intel MKL library, we need to set the following environment variable
+export MKL_DEBUG_CPU_TYPE=5
+
+# Change permissions for {un} directory on the VM storage
+export QC_RUN=/mnt/{un}/$SLURM_JOB_ID
+if [ -d $QC_RUN ]; then
+        sudo rm -vrf $QC_RUN
+else
+        sudo mkdir -p $QC_RUN
+fi
+
+sudo chmod 777 $QC_RUN
+
+
+# Now, copy the input file to the VM storage
+cp input.in $QC_RUN
+
+# Create a file to measure the time of execution
+touch initial_time
+
+# Change directory to the VM storage
+cd $QC_RUN
+
+# Run QChem
+qchem -slurm -nt {cpus} input.in output.out
+
+# Remove the scratch directory 
+sudo rm -vrf $SCRATCH
+
+# Copy all the files back to the current directory
+cp -vfr $QC_RUN/* "{pwd}"
+
+# Change directory back to the current directory
+cd "{pwd}"
+
+# Create a file to measure the time of execution
+touch final_time
+
+""",
+'molpro': """#!/bin/bash -l
+#SBATCH -p hpc,htc
+#SBATCH -J {name}
+#SBATCH --account={un}
+#SBATCH -N 1
+#SBATCH --ntasks-per-node={cpus}
+#SBATCH --mem={memory}
+#SBATCH -o out.txt
+#SBATCH -e err.txt
+
+# Load Molpro module
+module load easybuild/EasyBuild
+module load Molpro-2022.3.1
+
+# Set up scratch directory
+export SCRATCH=/mnt/{un}/scratch/molpro/{name}
+if [ -d $SCRATCH ]; then
+        sudo rm -rf $SCRATCH
+fi
+sudo mkdir -p $SCRATCH
+sudo chmod 777 $SCRATCH
+export MOLPRO_TMPDIR=$SCRATCH
+
+echo "============================================================"
+echo "Job ID : $SLURM_JOB_ID"
+echo "Job Name : $SLURM_JOB_NAME"
+echo "Starting on : $(date)"
+echo "Running on node : $SLURMD_NODENAME"
+echo "Current directory : $(pwd)"
+echo "============================================================"
+
+# Create a file to measure the time of execution
+touch initial_time
+
+# Get Current Directory
+export CWD={pwd}
+
+# Create a directory on the VM storage
+export MOLPRO_RUN=/mnt/{un}/molpro/$SLURM_JOB_ID
+sudo mkdir -p $MOLPRO_RUN
+sudo chmod 777 $MOLPRO_RUN
+
+# Now, copy the input file to the VM storage
+cp input.in $MOLPRO_RUN
+
+# Change directory to the VM storage
+cd $MOLPRO_RUN
+
+# Run Molpro
+molpro -n{cpus} -d $MOLPRO_TMPDIR input.in -o output.out
+
+sudo rm -rf $MOLPRO_TMPDIR
+
+# Copy all the files back to the current directory
+cp -vfr $MOLPRO_RUN/* {pwd}
+
+# Change directory back to the current directory
+cd {pwd}
+
+# Create a file to measure the time of execution
+touch final_time
+
+"""
+    },
+}
  'server3': {
         'gaussian': """#!/bin/bash -l
 #SBATCH -p normal
