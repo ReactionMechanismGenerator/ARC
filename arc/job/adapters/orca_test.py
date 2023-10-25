1f1b930497310803f6a2b5bb864b62b450e5fbbf
Lilachn91 <lilach.n1991@gmail.com>
2023-10-25 14:27:37 +0300

    Added tests for solvation methods with Orca 4, for work without parallelization

--- arc/job/adapters/orca_test.py
+++ arc/job/adapters/orca_test.py
@@ -3,6 +3,7 @@
 
 """
 This module contains unit tests of the arc.job.adapters.orca module
+For Orca version 4
 """
 
 import math
@@ -40,22 +41,51 @@ class TestOrcaAdapter(unittest.TestCase):
                                                            H      -0.53338088   -0.77135867   -0.54806440""")],
                                 testing=True,
                                 )
+        cls.job_2 = OrcaAdapter(execution_type='queue',
+                                job_type='sp',
+                                level=Level(method='DLPNO-CCSD(T)', basis='def2-tzvp', auxiliary_basis='def2-tzvp/c',
+                                            solvation_method='SMD', solvent='DMSO'),
+                                project='test',
+                                project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_OrcaAdapter'),
+                                species=[ARCSpecies(label='CH3O',
+                                                    xyz="""C       0.03807240    0.00035621   -0.00484242
+                                                           O       1.35198769    0.01264937   -0.17195885
+                                                           H      -0.33965241   -0.14992727    1.02079480
+                                                           H      -0.51702680    0.90828035   -0.29592912
+                                                           H      -0.53338088   -0.77135867   -0.54806440""")],
+                                testing=True,
+                                )
+        cls.job_3 = OrcaAdapter(execution_type='queue',
+                                job_type='sp',
+                                level=Level(method='DLPNO-CCSD(T)', basis='def2-tzvp', auxiliary_basis='def2-tzvp/c',
+                                            solvation_method='cpcm', solvent='water'),
+                                project='test',
+                                project_directory=os.path.join(ARC_PATH, 'arc', 'testing', 'test_OrcaAdapter'),
+                                species=[ARCSpecies(label='CH3O',
+                                                    xyz="""C       0.03807240    0.00035621   -0.00484242
+                                                           O       1.35198769    0.01264937   -0.17195885
+                                                           H      -0.33965241   -0.14992727    1.02079480
+                                                           H      -0.51702680    0.90828035   -0.29592912
+                                                           H      -0.53338088   -0.77135867   -0.54806440""")],
+                                testing=True,
+                                )
 
     def test_set_cpu_and_mem(self):
         """Test assigning number of cpu's and memory"""
-        self.job_1.cpu_cores = 48
+        # For work without parallelization, cpu_cores are set to 1 
+        self.job_1.cpu_cores = 1
         self.job_1.input_file_memory = None
         self.job_1.submit_script_memory = None
         self.job_1.set_cpu_and_mem()
-        self.assertEqual(self.job_1.cpu_cores, 48)
+        self.assertEqual(self.job_1.cpu_cores, 1)
 
     def test_set_input_file_memory(self):
         """Test setting the input_file_memory argument"""
-        expected_memory = math.ceil(14 * 1024 / 48)
+        expected_memory = math.ceil(14 * 1024 / 1)
         self.assertEqual(self.job_1.input_file_memory, expected_memory)
 
     def test_write_input_file(self):
-        """Test writing Gaussian input files"""
+        """Test writing ORCA input files"""
         self.job_1.write_input_file()
         with open(os.path.join(self.job_1.local_path, input_filenames[self.job_1.job_adapter]), 'r') as f:
             content_1 = f.read()
@@ -63,9 +93,9 @@ class TestOrcaAdapter(unittest.TestCase):
 ! NRSCF # using Newton–Raphson SCF algorithm 
 !sp 
 
-%maxcore 299
+%maxcore 8000
 %pal # job parallelization settings
-nprocs 48
+nprocs 1
 end
 %scf # recommended SCF settings
 NRMaxIt 400
@@ -83,6 +113,78 @@ H      -0.53338088   -0.77135867   -0.54806440
 *
 """
         self.assertEqual(content_1, job_1_expected_input_file)
+
+    def test_write_input_file_with_SMD_solvation(self):
+        """Test writing ORCA input files with SMD solvation"""
+        self.job_2.write_input_file()
+        with open(os.path.join(self.job_2.local_path, input_filenames[self.job_2.job_adapter]), 'r') as f:
+            content_2 = f.read()
+        job_2_expected_input_file = """!uHF dlpno-ccsd(t) def2-tzvp def2-tzvp/c tightscf normalpno 
+! NRSCF # using Newton–Raphson SCF algorithm 
+!sp 
+
+%maxcore 8000
+%pal # job parallelization settings
+nprocs 1
+end
+%scf # recommended SCF settings
+NRMaxIt 400
+NRStart 0.00005
+MaxIter 500
+end
+
+
+
+%cpcm SMD true
+       solvent "dmso"
+end
+            
+
+
+* xyz 0 2
+C       0.03807240    0.00035621   -0.00484242
+O       1.35198769    0.01264937   -0.17195885
+H      -0.33965241   -0.14992727    1.02079480
+H      -0.51702680    0.90828035   -0.29592912
+H      -0.53338088   -0.77135867   -0.54806440
+*
+"""
+        self.assertEqual(content_2, job_2_expected_input_file)
+
+    def test_write_input_file_with_CPCM_solvation(self):
+        """Test writing ORCA input files with CPCM solvation"""
+        self.job_3.write_input_file()
+        with open(os.path.join(self.job_3.local_path, input_filenames[self.job_3.job_adapter]), 'r') as f:
+            content_3 = f.read()
+        job_3_expected_input_file = """!uHF dlpno-ccsd(t) def2-tzvp def2-tzvp/c tightscf normalpno 
+! NRSCF # using Newton–Raphson SCF algorithm 
+!sp 
+
+%maxcore 8000
+%pal # job parallelization settings
+nprocs 1
+end
+%scf # recommended SCF settings
+NRMaxIt 400
+NRStart 0.00005
+MaxIter 500
+end
+
+
+
+!CPCM(water)
+            
+
+
+* xyz 0 2
+C       0.03807240    0.00035621   -0.00484242
+O       1.35198769    0.01264937   -0.17195885
+H      -0.33965241   -0.14992727    1.02079480
+H      -0.51702680    0.90828035   -0.29592912
+H      -0.53338088   -0.77135867   -0.54806440
+*
+"""
+        self.assertEqual(content_3, job_3_expected_input_file)
 
     def test_set_files(self):
         """Test setting files"""
