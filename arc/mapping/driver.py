ea26c8e5fbb7c970b0aeb1862f2b6ed689c3f96b
Alon Grinberg Dana <alon@technion.ac.il>
2024-07-03 08:13:12 +0300

    Don't use RMGDatabase to determine reaction family
    Currently the rmgdb module is only used by processor which will be dealt with in a different PR

--- arc/mapping/driver.py
+++ arc/mapping/driver.py
@@ -9,7 +9,6 @@ Strategy:
 
 from typing import TYPE_CHECKING, List, Optional
 
-import arc.rmgdb as rmgdb
 from arc.mapping.engine import (assign_labels_to_products,
                                 are_adj_elements_in_agreement,
                                 create_qc_mol,
@@ -64,7 +63,7 @@ def map_reaction(rxn: 'ARCReaction',
         return _map
     else:
         if rxn.family is None:
-            rmgdb.determine_family(reaction=rxn, db=db)
+            rxn.determine_family()
         if rxn.family is None:
             logger.warning(f'Could not determine the reaction family for {rxn.label}. Mapping as a general or isomerization reaction.')
             _map = map_general_rxn(rxn, backend=backend)
