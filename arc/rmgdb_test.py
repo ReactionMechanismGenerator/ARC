81a3d0c49c7bcf6b570d0178fd207f34b1c8acdf
Alon Grinberg Dana <alon@technion.ac.il>
2024-07-03 08:13:41 +0300

    Tests: Removed RMGDatabase from tests

--- arc/rmgdb_test.py
+++ arc/rmgdb_test.py
@@ -13,8 +13,6 @@ from rmgpy.reaction import Reaction
 from rmgpy.species import Species
 
 import arc.rmgdb as rmgdb
-from arc.reaction import ARCReaction
-from arc.species import ARCSpecies
 
 
 class TestRMGDB(unittest.TestCase):
@@ -43,41 +41,6 @@ class TestRMGDB(unittest.TestCase):
         self.assertAlmostEqual(spc.get_enthalpy(298), -393547.040000, 5)
         self.assertAlmostEqual(spc.get_entropy(298), 213.71872, 5)
         self.assertAlmostEqual(spc.get_heat_capacity(1000), 54.35016, 5)
-
-    def test_determine_family(self):
-        """Test the determine_family() function."""
-        rxn = ARCReaction(r_species=[ARCSpecies(label='CH2CH2CH3', smiles='[CH2]CC')],
-                          p_species=[ARCSpecies(label='CH3CHCH3', smiles='C[CH]C')])
-        self.assertIsNone(rxn.family)
-        rmgdb.determine_family(rxn, db=None)
-        self.assertEqual(rxn.family.label, 'intra_H_migration')
-
-        rxn = ARCReaction(r_species=[ARCSpecies(label='C2H6', smiles='CC'),
-                                     ARCSpecies(label='OH', smiles='[OH]')],
-                          p_species=[ARCSpecies(label='water', smiles='O'),
-                                     ARCSpecies(label='C2H5', smiles='[CH2]C')])
-        self.assertIsNone(rxn.family)
-        rmgdb.determine_family(rxn, db=self.rmgdb)
-        self.assertEqual(rxn.family.label, 'H_Abstraction')
-
-        r_1 = ARCSpecies(label='C3H6O', smiles='CCC=O')
-        r_2 = ARCSpecies(label='C4H9O', smiles='[CH2]C(C)CO')
-        p_1 = ARCSpecies(label='C3H5O', smiles='CC=C[O]')  # This is the "wrong" resonance structure on purpose.
-        p_2 = ARCSpecies(label='C4H10O', smiles='CC(C)CO')
-        rxn = ARCReaction(reactants=['C3H6O', 'C4H9O'], products=['C3H5O', 'C4H10O'],
-                          r_species=[r_1, r_2], p_species=[p_1, p_2])
-        self.assertIsNone(rxn.family)
-        rmgdb.determine_family(rxn, db=self.rmgdb)
-        self.assertEqual(rxn.family.label, 'H_Abstraction')
-
-        r_1 = ARCSpecies(label='C2H6', smiles='CC')
-        r_2 = ARCSpecies(label='CCOOj', smiles='CCO[O]')
-        p_1 = ARCSpecies(label='CCOOH', smiles='CCOO')
-        p_2 = ARCSpecies(label='C2H5', smiles='C[CH2]')
-        rxn = ARCReaction(r_species=[r_1, r_2], p_species=[p_1, p_2])
-        self.assertIsNone(rxn.family)
-        rmgdb.determine_family(rxn, db=self.rmgdb)
-        self.assertEqual(rxn.family.label, 'H_Abstraction')
 
     def test_determining_rmg_kinetics(self):
         """Test the determine_rmg_kinetics() function"""
