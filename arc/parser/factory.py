304a44c768abd78870c9aaaa9cea7c958c473539
Calvin Pieters <calvinpieters@gmail.com>
2025-06-25 07:25:19 +0300

    Refactor ess_factory to improve clarity and structure by moving determine_ess import and enhancing error handling

--- arc/parser/factory.py
+++ arc/parser/factory.py
@@ -5,7 +5,6 @@ A factory for parsing ESS log files.
 from typing import Optional, Type
 
 from arc.parser.adapter import ESSAdapter, ESSEnum
-from arc.parser.parser import determine_ess
 
 _registered_ess_adapters = {}  # keys are ESSEnum, values are ESSAdapter subclasses
 
@@ -24,7 +23,8 @@ def register_ess_adapter(ess_adapter_label: str,
         TypeError: If ess_adapter_class is not a subclass of ESSAdapter.
     """
     if not issubclass(ess_adapter_class, ESSAdapter):
-        raise TypeError(f'ESS adapter class {ess_adapter_class} is not a subclass ESSAdapter.')
+        raise TypeError(
+            f'ESS adapter class {ess_adapter_class} is not a subclass ESSAdapter.')
     _registered_ess_adapters[ESSEnum(ess_adapter_label.lower())] = ess_adapter_class
 
 
@@ -41,13 +41,17 @@ def ess_factory(log_file_path: str,
     Returns: ESSAdapter
         The requested ESSAdapter subclass, initialized with the ESS log file path.
     """
-    ess_adapter = ess_adapter or determine_ess(log_file_path=log_file_path)
-    if ess_adapter not in _registered_ess_adapters.keys():
-        raise ValueError(f'The "ess_adapter" argument of {ess_adapter} was not present in the keys for the '
-                         f'_registered_ess_adapters dictionary: {list(_registered_ess_adapters.keys())}'
-                         f'\nPlease check that the job adapter was registered properly.')
+    if ess_adapter is None:
+        from arc.parser.parser import determine_ess
+        ess_adapter = determine_ess(log_file_path)
+    
+    ess_enum = ESSEnum(ess_adapter.lower())
+    
+    if ess_enum not in _registered_ess_adapters.keys():
+        raise ValueError(
+            f'The "ess_adapter" argument of {ess_adapter} was not present in the keys for the '
+            f'_registered_ess_adapters dictionary: {list(_registered_ess_adapters.keys())}'
+            f'\nPlease check that the job adapter was registered properly.'
+        )
 
-    ess_adapter = ESSEnum(ess_adapter.lower())
-
-    ess_adapter_class = _registered_ess_adapters[ess_adapter](log_file_path=log_file_path)
-    return ess_adapter_class
+    return _registered_ess_adapters[ess_enum](log_file_path=log_file_path)
