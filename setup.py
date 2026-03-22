a9a880a6be03d20dd7eb5584dd574ac3346d647d
kfir4444 <kfir.k4@gmail.com>
2026-03-22 16:12:20 +0200

    Fix codecov coverage for Cython modules

--- setup.py
+++ setup.py
@@ -27,26 +27,32 @@ except ImportError:
 
 Options.annotate = False  # HTML annotation
 
+# Check if we are building for a coverage report
+BUILD_FOR_COVERAGE = os.environ.get("ARC_COVERAGE") == "1"
+
 directives = {
     'language_level': 3,
+    'linetrace': BUILD_FOR_COVERAGE,
 }
 
+macros = [('CYTHON_TRACE', '1'), ('CYTHON_TRACE_NOGIL', '1')] if BUILD_FOR_COVERAGE else []
+
 ext_modules = [
-    Extension('arc.molecule.atomtype', ['arc/molecule/atomtype.py'], include_dirs=['.']),
-    Extension('arc.constants', ['arc/constants.py'], include_dirs=['.']),
-    Extension('arc.molecule.element', ['arc/molecule/element.py'], include_dirs=['.']),
-    Extension('arc.molecule.graph', ['arc/molecule/graph.pyx'], include_dirs=['.']),
-    Extension('arc.molecule.group', ['arc/molecule/group.py'], include_dirs=['.']),
-    Extension('arc.molecule.molecule', ['arc/molecule/molecule.py'], include_dirs=['.']),
-    Extension('arc.molecule.symmetry', ['arc/molecule/symmetry.py'], include_dirs=['.']),
-    Extension('arc.molecule.vf2', ['arc/molecule/vf2.pyx'], include_dirs=['.']),
-    Extension('arc.molecule.converter', ['arc/molecule/converter.py'], include_dirs=['.']),
-    Extension('arc.molecule.translator', ['arc/molecule/translator.py'], include_dirs=['.']),
-    Extension('arc.molecule.util', ['arc/molecule/util.py'], include_dirs=['.']),
-    Extension('arc.molecule.inchi', ['arc/molecule/inchi.py'], include_dirs=['.']),
-    Extension('arc.molecule.resonance', ['arc/molecule/resonance.py'], include_dirs=['.']),
-    Extension('arc.molecule.pathfinder', ['arc/molecule/pathfinder.py'], include_dirs=['.']),
-    Extension('arc.molecule.kekulize', ['arc/molecule/kekulize.pyx'], include_dirs=['.']),
+    Extension('arc.molecule.atomtype', ['arc/molecule/atomtype.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.constants', ['arc/constants.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.element', ['arc/molecule/element.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.graph', ['arc/molecule/graph.pyx'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.group', ['arc/molecule/group.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.molecule', ['arc/molecule/molecule.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.symmetry', ['arc/molecule/symmetry.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.vf2', ['arc/molecule/vf2.pyx'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.converter', ['arc/molecule/converter.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.translator', ['arc/molecule/translator.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.util', ['arc/molecule/util.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.inchi', ['arc/molecule/inchi.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.resonance', ['arc/molecule/resonance.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.pathfinder', ['arc/molecule/pathfinder.py'], include_dirs=['.'], define_macros=macros),
+    Extension('arc.molecule.kekulize', ['arc/molecule/kekulize.pyx'], include_dirs=['.'], define_macros=macros),
 ]
 
 if 'main' in sys.argv:
