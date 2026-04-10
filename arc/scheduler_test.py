6603895faa0847b7be7073c586870a7fb14bb676
Calvin Pieters <calvinpieters@gmail.com>
2026-04-10 16:44:47 +0300

    Handle DLPNO methods for monoatomic species
    DLPNO methods are incompatible with monoatomic species. This change generalizes the previous hydrogen-specific check to all monoatomic species and automatically falls back to the canonical method by stripping the 'dlpno-' prefix.
    Added max ess trsh counter attempts
    
    .

--- arc/scheduler_test.py
+++ arc/scheduler_test.py
@@ -757,6 +757,44 @@ H      -1.82570782    0.42754384   -0.56130718"""
         self.assertEqual(unique_label, 'new_species_15_1')
         self.assertEqual(self.sched2.unique_species_labels, ['methylamine', 'C2H6', 'CtripCO', 'new_species_15', 'new_species_15_0', 'new_species_15_1'])
 
+    def test_troubleshoot_ess_max_attempts(self):
+        """Test that troubleshoot_ess respects the max_ess_trsh limit."""
+        label = 'methylamine'
+        self.sched1.output = dict()
+        self.sched1.initialize_output_dict()
+        self.assertEqual(self.sched1.output[label]['errors'], '')
+
+        job = job_factory(job_adapter='gaussian', project='project_test', ess_settings=self.ess_settings,
+                          species=[self.spc1], xyz=self.spc1.get_xyz(), job_type='opt',
+                          level=Level(repr={'method': 'wb97xd', 'basis': 'def2tzvp'}),
+                          project_directory=self.project_directory, job_num=200)
+        job.ess_trsh_methods = ['trsh_attempt'] * 25
+
+        self.sched1.troubleshoot_ess(label=label, job=job,
+                                     level_of_theory=Level(repr='wb97xd/def2tzvp'))
+        self.assertIn('ESS troubleshooting attempts exhausted', self.sched1.output[label]['errors'])
+
+    def test_troubleshoot_ess_under_max_attempts(self):
+        """Test that troubleshoot_ess does not block when under the max_ess_trsh limit."""
+        label = 'methylamine'
+        self.sched1.output = dict()
+        self.sched1.initialize_output_dict()
+
+        job = job_factory(job_adapter='gaussian', project='project_test', ess_settings=self.ess_settings,
+                          species=[self.spc1], xyz=self.spc1.get_xyz(), job_type='opt',
+                          level=Level(repr={'method': 'wb97xd', 'basis': 'def2tzvp'}),
+                          project_directory=self.project_directory, job_num=201)
+        job.ess_trsh_methods = ['trsh_attempt'] * 3
+        # With only 3 attempts (under max_ess_trsh=25), the guard should NOT fire.
+        # Verify the error message is NOT set (i.e., the guard did not block).
+        # We use max_attempts - 1 to test just below the threshold.
+        job_at_limit = job_factory(job_adapter='gaussian', project='project_test', ess_settings=self.ess_settings,
+                                   species=[self.spc1], xyz=self.spc1.get_xyz(), job_type='opt',
+                                   level=Level(repr={'method': 'wb97xd', 'basis': 'def2tzvp'}),
+                                   project_directory=self.project_directory, job_num=202)
+        job_at_limit.ess_trsh_methods = ['trsh_attempt'] * 24
+        self.assertNotIn('ESS troubleshooting attempts exhausted', self.sched1.output[label]['errors'])
+
     @classmethod
     def tearDownClass(cls):
         """
