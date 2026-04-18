109bacf531211ba3a852a07f2a2deb57a41c363c
Alon Grinberg Dana <alon@technion.ac.il>
2026-04-18 14:13:20 +0300

    Tests: proper teardown of molpro tests

--- arc/job/adapters/molpro_test.py
+++ arc/job/adapters/molpro_test.py
@@ -468,8 +468,9 @@ int;
         A function that is run ONCE after all unit tests in this class.
         Delete all project directories created during these unit tests
         """
-        for folder in ['test_MolproAdapter_1', 'test_MolproAdapter_2']:
-            shutil.rmtree(os.path.join(ARC_TESTING_PATH, folder), ignore_errors=True)
+        for attr in vars(cls).values():
+            if isinstance(attr, MolproAdapter):
+                shutil.rmtree(attr.project_directory, ignore_errors=True)
 
 
 if __name__ == '__main__':
